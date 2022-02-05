c1955d4f8bad1b21b460c9136f8e1e07b661378a
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-02-05 17:50:10 -0500

    Added main container + description

--- src/Search.js
+++ src/Search.js
@@ -6,10 +6,15 @@ export default function Search() {
 return (
   <div className="Search">
     <div className="container bor">
-      <div className="container bor">
-        <div className="row bor">
-          <div className="col bor">
-            <h1 className="title">Dictionary</h1>
+      <div className="row bor">
+        <div className="col-12 d-flex justify-content-center main-container bor">
+          <div className="row bor">
+            <div className="col-12 bor">
+              <h1 className="title">Dictionary</h1>
+            </div>
+            <div className="col-12 bor">
+              <p className="description">What are you looking for?</p>
+            </div>
           </div>
         </div>
       </div>

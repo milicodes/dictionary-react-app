e83e57f0dbeffa0c45c9798381e08c6499e50936
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-02-05 16:49:09 -0500

    Created search component and added fonts

---
+++ src/Search.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Search.css"
+
+
+export default function Search() {
+return (
+  <div className="Search">
+    <div className="container bor">
+      <div className="container bor">
+        <div className="row bor">
+          <div className="col bor">
+            <h1 className="title">Dictionary</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+}

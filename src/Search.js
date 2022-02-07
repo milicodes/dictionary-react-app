09e73aa6e42169dc97db73970158d99fb54d63d4
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-02-06 20:44:11 -0500

    Added state to update search bar value + alert the value

--- src/Search.js
+++ src/Search.js
@@ -1,32 +1,40 @@
-import React from "react"
-import "./Search.css"
-
-
+import React, { useState } from "react";
+import "./Search.css";
 export default function Search() {
-return (
-  <div className="Search">
-    <div className="container bor">
-      <div className="row bor">
-        <div className="col-12 d-flex justify-content-center main-container bor">
-          <div className="row bor">
-            <div className="col-12 bor">
-              <h1 className="title">Dictionary</h1>
-            </div>
-            <div className="col-12 bor">
-              <p className="description">What are you looking for?</p>
-            </div>
-            <div className="col-12 bor" align="center" autofocus={true}>
-              <form>
-                <input type="search" />
-                <button type="button" class="btn btn-light">
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition...`);
+  }
+
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Search">
+      <div className="container bor">
+        <div className="row bor">
+          <div className="col-12 d-flex justify-content-center main-container bor">
+            <div className="row bor">
+              <div className="col-12 bor">
+                <h1 className="title">Dictionary</h1>
+              </div>
+              <div className="col-12 bor">
+                <p className="description">What are you looking for?</p>
+              </div>
+              <div className="col-12 bor" align="center" autoFocus={true}>
+                <form onSubmit={search}>
+                  <input type="search" onChange={handleKeyword} /> {""}
+                  <button type="button" className="btn btn-light">
                     🔍
-                </button>
-              </form>
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 }

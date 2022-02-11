e139ad94eb63cffef1be72a90bb9fe46f26e42d6
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-02-11 18:50:10 -0500

    Added phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  return (
+    <div className="Phonetics">
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(promps) {
   if (promps.results) {
     return (
       <div className="container Results">
         <div className="row">
+          {/*Word */}
           <div className="col-6 bor">
             <h2 className="definition">{promps.results.word}</h2>
+            {/*Phonetics */}
+            {promps.results.phonetic.map(function(phonetic, index) {
+              return (<div key={index}>
+                <Phonetics phonetic={phonetic} />
+              </div>)
+            })}
           </div>
           <div className="col-6 bor"></div>
           <div className="col-6 bor">

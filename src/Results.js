c6d902671d55d4decc1dc87d743fbc583c5bfd08
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-03-03 19:16:20 -0500

    Added background effect + animation

--- src/Results.js
+++ src/Results.js
@@ -2,6 +2,7 @@ import React from "react";
 import Meaning from "./Meaning";
 import Phonetics from "./Phonetics";
 import PhoneticsText from "./PhoneticsText";
+import "./Results.css";
 
 export default function Results(promps) {
   if (promps.results) {

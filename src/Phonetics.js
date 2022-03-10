55f10a21ee8ab79918dc97b06f815c8fa5b29eab
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-03-10 18:41:35 -0500

    Up

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,7 +5,7 @@ import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 
 export default function Phonetics(props) {
   let phonetic = props.phonetic.audio;
-  if (phonetic) {
+  if (phonetic) { 
     return (
       <span className="col-2 speaker-icon Phonetics">
         <a href={phonetic} classnametarget="speaker-color" target="_blank" rel="noreferrer">
